54f689ef5b9ca76d3374fde7e38e25f9123eaf99
kanik-snippet <bunnygupta578@gmail.com>
2026-08-26 18:40:24 +0530

    Deploy Quant static assets to public root

--- deploy/rootless-install-quant.sh
+++ deploy/rootless-install-quant.sh
@@ -6,5 +6,6 @@ export APP_DIR
 export SUPERVISOR_CONFIG_OVERRIDE="${SUPERVISOR_CONFIG_OVERRIDE:-$APP_DIR/deploy/supervisord-quant.conf}"
 export HEALTH_PORT="${HEALTH_PORT:-8091}"
 export APP_LABEL="${APP_LABEL:-Quant Tool}"
+export PUBLIC_STATIC_DIR="${PUBLIC_STATIC_DIR:-$HOME/htdocs/exchange.api-grid.com/static}"
 
 exec "$APP_DIR/deploy/rootless-install.sh"
